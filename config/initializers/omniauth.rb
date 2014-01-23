173d95991257015eecd41d49deaac08adb135058
Rob Forman <rob@robforman.com>
2014-01-22 23:49:53 -0500

    env not present at boot on heroku?

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :trello, ENV.fetch('TRELLO_KEY'), ENV.fetch('TRELLO_SECRET'),
+  provider :trello, ENV['TRELLO_KEY'], ENV['TRELLO_SECRET'],
     app_name: "Trello Tracker", scope: 'read,write', expiration: 'never'
 end

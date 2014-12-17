10d6aab167b0564ebd20be6207a5fc407fc1dfc1
Rob Forman <rob@robforman.com>
2014-12-17 02:44:04 -0500

    code climate is addictive

--- lib/trello_webhook_action.rb
+++ lib/trello_webhook_action.rb
@@ -14,7 +14,7 @@ class TrelloWebhookAction
 
     case action["type"]
       when "updateCard"
-        return :card_list_change if has_listBefore? && has_listAfter?
+        return :card_list_change if list_before.present? && list_after.present?
         return :card_name_change if was_renamed?
         return :archive_card if was_closed?
       when "createCard"
@@ -25,45 +25,45 @@ class TrelloWebhookAction
   end
 
   def before_list_id
-    acceptable_types = [:card_list_change]
-    raise StandardError.new("Mismatch change_type: #{change_type}") unless acceptable_types.include?(change_type)
-    action.fetch("data").fetch("listBefore").fetch("id")
+    ensure_change_type([:card_list_change])
+    list_before.fetch("id")
   end
 
   def after_list_id
-    acceptable_types = [:card_list_change]
-    raise StandardError.new("Mismatch change_type: #{change_type}") unless acceptable_types.include?(change_type)
-    action.fetch("data").fetch("listAfter").fetch("id")
+    ensure_change_type([:card_list_change])
+    list_after.fetch("id")
   end
 
   def card_id
-    acceptable_types = [:card_name_change, :create_card, :delete_card, :archive_card]
-    raise StandardError.new("Mismatch change_type: #{change_type}") unless acceptable_types.include?(change_type)
-    action.fetch("data").fetch("card").fetch("id")
+    ensure_change_type([:card_name_change, :create_card, :delete_card, :archive_card])
+    data.fetch("card").fetch("id")
   end
 
   def list_id
-    acceptable_types = [:create_card, :delete_card]
-    raise StandardError.new("Mismatch change_type: #{change_type}") unless acceptable_types.include?(change_type)
-    action.fetch("data").fetch("list").fetch("id")
+    ensure_change_type([:create_card, :delete_card])
+    data.fetch("list").fetch("id")
   end
 
   private
 
-  def card_data
+  def ensure_change_type(acceptable_types)
+    raise StandardError.new("Mismatch change_type: #{change_type}") unless acceptable_types.include?(change_type)
+  end
+
+  def data
     action.fetch("data", {})
   end
 
+  def list_before
+    data.fetch("listBefore", {})
+  end
+
+  def list_after
+    data.fetch("listAfter", {})
+  end
+
   def old_data
-    card_data.fetch("old", {})
-  end
-
-  def has_listBefore?
-    card_data.has_key?("listBefore")
-  end
-
-  def has_listAfter?
-    card_data.has_key?("listAfter")
+    data.fetch("old", {})
   end
 
   def was_renamed?
